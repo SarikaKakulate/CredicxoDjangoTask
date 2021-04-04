e0de82084ff5b308a8dc92b1a5eb20bcbb6fd7c2
sarika <kakulatesarika144@gmail.com>
2021-04-04 12:39:54 +0530

    added student model

--- Task/Users/models.py
+++ Task/Users/models.py
@@ -11,16 +11,27 @@ class User(AbstractUser):
       ('student', 'student'),
       ('teacher', 'teacher'),
     )
-    
+
     username     = models.CharField(_("Username"), blank=False, null=False, max_length=255, unique=True)
     first_name   = models.CharField(_("First Name"), blank=False, null=False, max_length=255)
     last_name    = models.CharField(_("Last Name"), blank=False, null=False, max_length=255)
     email        = models.EmailField(_('Email'), blank=False, null=False, unique=True)
-    user_level   = models.CharField(_('User Level'), choices=USER_LEVEL_CHOICES)
+    user_level   = models.CharField(_('User Level'), choices=USER_LEVEL_CHOICES, blank=False, null=False, max_length=50)
    
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = ['email']
 
     def __str__(self):
         return self.username
+
+
+class Student(models.Model):
+    
+    first_name      = models.CharField(_("First Name"), blank=False, null=False, max_length=255)
+    last_name       = models.CharField(_("Last Name"), blank=False, null=False, max_length=255)
+    student_class   = models.CharField(_("Student Class"), blank=False, null=False, max_length=255)
+    student_roll    = models.CharField(_("Student Roll"), blank=False, null=False, max_length=255)
+    
+    def __str__(self):
+        return self.first_name
 
