630275b2d2f2ba41dcd7b83c50de208a443b2d41
sarika <kakulatesarika144@gmail.com>
2021-04-04 12:24:53 +0530

    registered user model to admin

--- Task/Users/admin.py
+++ Task/Users/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.contrib.auth import admin as auth_admin
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Register your models here.
+
+
+@admin.register(User)
+class UserAdmin(auth_admin.UserAdmin):
+
+    fieldsets = (
+        ("User info", {
+            "fields": ("username", "password",)
+        }),
+        ("Personal info", {
+            "fields": ("first_name", "last_name", "email",)
+        }),
+        ("Permissions", {
+           'fields': ('is_active', 'is_staff', 'is_superuser', 'user_level', 'groups', 'user_permissions'),
+        }),
+        ('Important dates', {
+            'fields': ('last_login', 'date_joined'),
+        }),
+    )
+
+    list_display = ["id", "username", "first_name", "last_name", "email", "is_superuser"]
+    list_filter = ['user_level', 'is_superuser']
+    search_fields = ["username", "email"]
+    readonly_fields = []
