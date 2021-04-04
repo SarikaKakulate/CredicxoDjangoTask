fd1af9ad0c9ba6a1eec2594593f4d2386e8ef03a
sarika <kakulatesarika144@gmail.com>
2021-04-04 12:19:10 +0530

    added user model

--- Task/Users/models.py
+++ Task/Users/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+
+
+class User(AbstractUser):
+
+    USER_LEVEL_CHOICES = (
+      ('student', 'student'),
+      ('teacher', 'teacher'),
+    )
+    
+    username     = models.CharField(_("Username"), blank=False, null=False, max_length=255, unique=True)
+    first_name   = models.CharField(_("First Name"), blank=False, null=False, max_length=255)
+    last_name    = models.CharField(_("Last Name"), blank=False, null=False, max_length=255)
+    email        = models.EmailField(_('Email'), blank=False, null=False, unique=True)
+    user_level   = models.CharField(_('User Level'), choices=USER_LEVEL_CHOICES)
+   
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    def __str__(self):
+        return self.username
+
