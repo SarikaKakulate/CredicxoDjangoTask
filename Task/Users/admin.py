0653a85d422fbc64902a907bf4b338cc544979dc
sarika <kakulatesarika144@gmail.com>
2021-04-04 12:40:48 +0530

    registered student model to admin

--- Task/Users/admin.py
+++ Task/Users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth import admin as auth_admin
 from django.contrib.auth import get_user_model
+from Users.models import Student
 
 User = get_user_model()
 
@@ -25,7 +26,27 @@ class UserAdmin(auth_admin.UserAdmin):
         }),
     )
 
-    list_display = ["id", "username", "first_name", "last_name", "email", "is_superuser"]
+    list_display = ["id", "username", "email", "is_superuser"]
     list_filter = ['user_level', 'is_superuser']
     search_fields = ["username", "email"]
     readonly_fields = []
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "first_name",
+        "last_name",
+        "student_class",
+        "student_roll",
+    ]
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return True
+
+    def has_delete_permission(self, request, obj=None):
+        return False
